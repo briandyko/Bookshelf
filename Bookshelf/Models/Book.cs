0162fa9ea3e7bbe93d64c8865c1d5f38167e5d40
briandyko <briandyko@gmail.com>
2017-10-26 15:16:36 -0400

    added buttons and bootstrap tables

--- Bookshelf/Models/Book.cs
+++ Bookshelf/Models/Book.cs
@@ -11,14 +11,16 @@ namespace Bookshelf.Models
     {
         [Key]
         public int ID { get; set; }
-        [MinLength(2),MaxLength(50)]
+        [MinLength(2), MaxLength(50)]
         public string Title { get; set; }
+        [Display(Name = "Published Date")]
         public DateTime PublishedDate { get; set; }
         [MaxLength(500)]
         public string Description { get; set; }
+        //[Required]
         public string ISBN { get; set; }
 
-        [ForeignKey("Category")]
+        [ForeignKey("Category"), Display(Name ="Category")]
         public int CategoryID { get; set; }
         public virtual Category Category { get; set; }
     }
